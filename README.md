3658e26e2034480b2fcaaa7c4785bb76bb8d75dd
pherbish <pherbishorwa@gmail.com>
2024-03-10 09:53:20 -0700

    creating baseclass

---
+++ README.md
@@ -0,0 +1,8 @@
+The project is web application for AIRbnb rooms, Its a full web with both front end and backend.
+
+It has a commanline that aids in :
+Create a new object (ex: a new User or a new Place)
+Retrieve an object from a file, a database etc…
+Do operations on objects (count, compute stats, etc…)
+Update attributes of an object
+Destroy an objec
---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""The base class of the models"""
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel:
+    """This is the parent class for all the models"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """ 
+        """
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        """
+        """
+        Ins_v = self.__dict__.copy()
+        Ins_v["__class__"] = self.__class__.__name__
+        Ins_v["created_at"] = self.created_at.isoformat()
+        Ins_v["updated_at"] = self.updated_at.isoformat()
+        return Ins_v
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+
