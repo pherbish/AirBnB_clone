1209bbf7148d8d6e9b5fccdc8f2a7c932c34dacb
pherbish <pherbishorwa@gmail.com>
2024-03-10 15:03:45 -0700

    cmd implementation

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand console class
+    """
+    prompt = "(hbnb) "
+     def do_EOF(self, arg):
+        """
+        EOF (Ctrl+D) signal to exit the program.
+        """
+        return True
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        return True
+    def emptyline(self):
+        """
+        Do nothing when an empty line is entered.
+        """
+        pass
+
+    if __name__ == '__main__':
+    HBNBCommand().cmdloop()
