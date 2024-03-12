5dc97c5f3ac4992bb4d8470f1ba1e29c04d6cfc6
pherbish <pherbishorwa@gmail.com>
2024-03-12 00:31:02 -0700

    class user

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ import shlex
 import ast
 from models import storage
 from models.base_model import BaseModel
-#from models.user import User
+from models.user import User
 #from models.amenity import Amenity
 #from models.place import Place
 #from models.review import Review
