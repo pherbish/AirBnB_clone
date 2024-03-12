8bcda8898a354cb571e8c5f7798a06b4bfcc7627
pherbish <pherbishorwa@gmail.com>
2024-03-12 00:42:22 -0700

    classes inheriting from Basemodel

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,12 +3,12 @@
 import os
 import json
 from models.base_model import BaseModel
-#from models.user import User
-#from models.amenity import Amenity
-#from models.place import Place
-#from models.review import Review
-#from models.state import State
-#from models.city import City
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 class FileStorage:
     """Class for soring objects"""
