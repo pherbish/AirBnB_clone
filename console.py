3b21441b4a099b4893c115ca8a2d41efcea7c40c
pherbish <pherbishorwa@gmail.com>
2024-03-12 00:45:06 -0700

    classes inheriting from Basemodel

--- console.py
+++ console.py
@@ -7,11 +7,11 @@ import ast
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-#from models.amenity import Amenity
-#from models.place import Place
-#from models.review import Review
-#from models.state import State
-#from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 def split_curly_braces(e_arg):
